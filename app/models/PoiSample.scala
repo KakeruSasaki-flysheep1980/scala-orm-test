c22427884655525328eea7cd71f3987be256a998
flysheep1980 <flysheep1980@gmail.com>
2013-07-13 22:26:23 +0900

    'poipoi'

---
+++ app/models/PoiSample.scala
@@ -0,0 +1,12 @@
+package models
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: flysheep1980
+ * Date: 13/07/13
+ * Time: 22:20
+ * To change this template use File | Settings | File Templates.
+ */
+class PoiSample {
+
+}
