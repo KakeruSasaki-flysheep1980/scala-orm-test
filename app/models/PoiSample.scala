01f2a8720bacc9345c1d70cc83d9a22804ed5826
flysheep1980 <flysheep1980@gmail.com>
2013-07-14 18:17:05 +0900

    pppppoi

--- app/models/PoiSample.scala
+++ app/models/PoiSample.scala
@@ -21,9 +21,48 @@ object PoiSample {
 
     Workbook(path).map {
       case \/-(workbook) => {
-        val book = workbook.asPoi
-        println(s"sheetNumber - ${book.getNumberOfSheets}")
-        book.getSheet("user").getRow(1).getCell(1)
+//        import xerial.lens._
+//        val s = workbook.sheets.head
+//        val t = ObjectType(s).rawType
+//        ObjectSchema(t).methods.foreach(println)
+
+        workbook.sheets.map { case Sheet(sheetName, rows) =>
+          val a = rows.map { case Row(index, cells) =>
+            println(s"rowIndex: ${index}")
+            cells.flatMap { cell =>
+              println(s"colIndex: ${cell.index}")
+              cell match {
+                case c: StringCell => Some(c.data)
+                case c: NumericCell => Some(c.data)
+                case c: BooleanCell => Some(c.data)
+                case c: FormulaCell => Some(c.data)
+                case _ => None
+              }
+            }
+          }
+
+          println(a)
+
+
+//          println(s"sheetName: ${sheetName}")
+//
+//          rows.map { case Row(index, cells) =>
+//            println(s"rowIndex: ${index}")
+//
+//            cells.map {
+//              case c: StringCell => println(c.data)
+//              case c: NumericCell => println(c.data)
+//              case c: BooleanCell => println(c.data)
+//              case c: FormulaCell => println(c.data)
+//            }
+//          }
+        }
+
+        true
+
+//        val book = workbook.asPoi
+//        println(s"sheetNumber - ${book.getNumberOfSheets}")
+//        book.getSheet("user").getRow(1).getCell(1)
       }
       case -\/(t) => throw t
     }.unsafePerformIO()
